eef3f57eec4364dced37c553c691fc0ea1f00e43
TheMopper <Haven.Smith384@gmail.com>
2018-07-25 09:28:52 -0500

    Game works like a charm!

--- simon_game.py
+++ simon_game.py
@@ -6,104 +6,66 @@ import LEDRGB as LED
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
+#Buzzer stuff
 buzz_pin = 32
 GPIO.setup(buzz_pin, GPIO.OUT)
 Buzz = GPIO.PWM(buzz_pin, 1000)
+frequencies = [220, 440, 880, 1760]
 
 colors = ['R', 'G', 'B', 'Y']
 colorList = []
-guessedColors = []
+gameOver = False
 
-frequencies = [220, 440, 880, 1760]
+#LED stuff
 R_pin = 11
 G_pin = 12
 B_pin = 13
-
-turn = 1
-
 LED.setup(R_pin, G_pin, B_pin)
 
-def blinkColors():
+
+
+def addNewColor():
 	n = random.randint(0, 3)
-	#colors.append(colors[n])
-	#color_string = ' '.join(colors)
-	#for i in range(0, len(colors)):
-		#print colors[i].lower()
-		#time.sleep(1)
 	colorList.append(colors[n])
-
-	
-	Buzz.ChangeFrequency(frequencies[n])
-	Buzz.start(50)
-	
-	print colors[n]
-
-	LED.setColor(colors[n])
-	time.sleep(0.5)
-	Buzz.stop()
-
-	LED.noColor()
-	time.sleep(0.5)
 
 def playColorList():
 	for i in range(0, len(colorList)):
 		Buzz.ChangeFrequency(220 * (2 ** colors.index(colorList[i])))
 		Buzz.start(50)
 		LED.setColor(colorList[i])
+		#print colorList[i]
 		time.sleep(0.5)
 		Buzz.stop()
 		LED.noColor()
 		time.sleep(0.5)
-
-def addNewColor():
-	n = random.randint(0, 3)
-	colorList.append(colors[n])
+	print ' '
 
 def guessColors():
-	guessNumber = 0
-	guessedColor = raw_Input()
-	if guessedColor == 'r' or guessedColor == 'g' or guessedColor == 'b' or guessedColor == 'y':
-		guessedColors.append(guessedColor)
-
-		if guessedColors[guessNumber] != colorList[Number]:
-			print "GAME OVER!"
-		
+	print "Start Guessing!"
+	Buzz.ChangeFrequency(300)
+	Buzz.start(50)
+	time.sleep(0.2)
+	Buzz.stop()
+	
+	guessString = raw_input()
+	if list(guessString.upper()) == colorList:
+		print "Correct"
+		return True
 	else:
-		print "Invalid input, please try again"
-
+		print "Oh No!"
+		return False
+	
 
 if __name__ == '__main__':
 	try:
-		while True:
+		addNewColor()
+		playColorList()
+		while guessColors():
 			addNewColor()
 			playColorList()
 			time.sleep(1.5)
+		print "You Lost!"
 	except KeyboardInterrupt:
 		LED.destroy()
 		print ' See ya later, Alligator'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
